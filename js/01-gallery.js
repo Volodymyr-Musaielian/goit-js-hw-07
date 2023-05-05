470623a483d072e1c68628798b6231514d13e161
Volodymyr-Musaielian <121056577+Volodymyr-Musaielian@users.noreply.github.com>
2023-05-05 10:16:28 +0300

    add 01-task

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createImageMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createImageMarkup(images) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  window.addEventListener("keydown", closeBtnEsc);
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const originalImage = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  originalImage.show();
+
+  function closeBtnEsc(event) {
+    if (event.code === "Escape") {
+      originalImage.close();
+    }
+  }
+}
